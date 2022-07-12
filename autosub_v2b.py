ec7eff3fbc8ee813fbc5c07850262758fbedd412
Ariel <107245128+arielherself@users.noreply.github.com>
2022-07-12 17:53:46 +0800

    Update autosub_v2b.py

--- autosub_v2b.py
+++ autosub_v2b.py
@@ -10,7 +10,8 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101
 home_urls = (
     'https://sub.chbjpw.mobi', 
     'https://cooc.cloud',
-    'https://xf.gl'
+    'https://xf.gl',
+    'https://konan.ml',
 )
 times = 15
 proxies = {
