1c0c7292414dc336d5cf3c71660f03e177cdc11c
yangyucacaa <51358108+yangyucacaa@users.noreply.github.com>
2022-07-16 17:01:10 +0800

    Update autogmail.py

--- autogmail.py
+++ autogmail.py
@@ -10,7 +10,7 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101
 home_urls = (
    # 'https://cooc.cloud',
     #'https://baipiao.buzz',
-    'https://circlecloud123.com',
+  #  'https://circlecloud123.com',
     'https://hmkj3.com',
     'https://sub.chbjpw.mobi',
     'https://acec.icu',
