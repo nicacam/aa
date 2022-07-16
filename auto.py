b2f7aba30de5a5fbe66faa3a3ac3bef87279a666
yangyucacaa <51358108+yangyucacaa@users.noreply.github.com>
2022-07-16 16:13:34 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -9,7 +9,6 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101
 
 home_urls = (
     'https://cooc.cloud',
-    'https://www.speedasia.xyz',
     'https://baipiao.buzz',
  #   'https://konan.ml',
 )
